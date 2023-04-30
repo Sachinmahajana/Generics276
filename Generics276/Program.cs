95f0a6a8d2ebc63a3fc216170da788ba981b67be
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-30 14:29:29 +0530

    [Sachin] Adding Welcome message

---
+++ Generics276/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Generics276
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Generic Programs");
+        }
+    }
+}
