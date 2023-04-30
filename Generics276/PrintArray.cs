8d96aae9acb04bc52a3751a765bcc439ee139385
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-30 14:59:25 +0530

    [Sachin] Adding UC1-Printarray

---
+++ Generics276/PrintArray.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics276
+{
+    internal class PrintArray
+    {
+        public static void ToPrint(int[] intArray)
+        {
+            Console.WriteLine("integers Array elements");
+            foreach(int element in intArray)
+            {
+                Console.WriteLine(element + " ");
+            }
+        }
+        public static void ToPrint(double[] doubleArray)
+        {
+            Console.WriteLine("double array elements");
+            foreach(double element in doubleArray)
+            {
+                Console.WriteLine(element + " ");
+            }
+        }
+        public static void ToPrint(char[] charArray)
+        {
+            Console.WriteLine("char array elements");
+            foreach(char element in charArray)
+            {
+                Console.WriteLine(element + " ");
+            }
+        }
+
+    }
+}
--- Generics276/Program.cs
+++ Generics276/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Generic Programs");
+            Console.WriteLine("Select the program given below");
+            Console.WriteLine("\n1:SimpleArray");
+            int option = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = { 2, 3, 4, 5 };
+            double[] doubleArray = { 2.3, 4.5, 5.7, 7.8 };
+            char[] charArray = { 'A', 'N', 'D', 'L' };
+            switch (option)
+            {
+                case 1:
+                    PrintArray.ToPrint(intArray);
+                    PrintArray.ToPrint(doubleArray);
+                    PrintArray.ToPrint(charArray);
+                    break;          
+            }
         }
     }
 }
