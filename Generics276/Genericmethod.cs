d02ed3316cea87447a88ba6659ce24bb13463c71
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-30 15:26:04 +0530

    [Sachin] Adding UC2-PrintarrayByusingGenericMethod

---
+++ Generics276/Genericmethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics276
+{
+    internal class Genericmethod
+    {
+        public static void ToPrint<T>(T[] intArray)
+        {
+            Console.WriteLine("Displaying Array elements");
+            foreach(T element in intArray)
+            {
+                Console.WriteLine(element + " "); 
+            }
+        }
+    }
+}
--- Generics276/Program.cs
+++ Generics276/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to the Generic Programs");
             Console.WriteLine("Select the program given below");
-            Console.WriteLine("\n1:SimpleArray");
+            Console.WriteLine("\n1:SimpleArray\n2:Genericmethod");
             int option = Convert.ToInt32(Console.ReadLine());
             int[] intArray = { 2, 3, 4, 5 };
             double[] doubleArray = { 2.3, 4.5, 5.7, 7.8 };
@@ -17,7 +17,15 @@
                     PrintArray.ToPrint(intArray);
                     PrintArray.ToPrint(doubleArray);
                     PrintArray.ToPrint(charArray);
-                    break;          
+                    break;
+                case 2:
+                    Genericmethod.ToPrint<int>(intArray);
+                    Genericmethod.ToPrint<double>(doubleArray);
+                    Genericmethod.ToPrint<char>(charArray);
+                    break;
+                default:
+                    Console.WriteLine("Please select the program given below");
+                    break;
             }
         }
     }
