ccc3054fdb4f1763702f5295eb045f7cbe7b99a2
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-30 23:06:22 +0530

    [Sachin] Adding UC4-GenericclassandmethodPrintarray

---
+++ Generics276/GenericmethodAndclass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics276
+{
+    internal class GenericmethodAndclass<E>
+    {
+        public static void ToPrint(E[] arr)
+        {
+            Console.WriteLine("Array Elements");
+            foreach(E e in arr)
+            {
+                Console.WriteLine(e + " ");
+            }
+        }
+    }
+}
--- Generics276/Program.cs
+++ Generics276/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to the Generic Programs");
             Console.WriteLine("Select the program given below");
-            Console.WriteLine("\n1:SimpleArray\n2:Genericmethod\n3:Gnericclass");
+            Console.WriteLine("\n1:SimpleArray\n2:Genericmethod\n3:Gnericclass\n4:GenericmethodAndclass");
             int option = Convert.ToInt32(Console.ReadLine());
             int[] intArray = { 2, 3, 4, 5 };
             double[] doubleArray = { 2.3, 4.5, 5.7, 7.8 };
@@ -30,6 +30,11 @@
                     doubleObj.Toprint();
                     Genericclass<char> charObj = new Genericclass<char>(charArray);
                     charObj.Toprint();
+                    break;
+                case 4:
+                    GenericmethodAndclass<int>.ToPrint(intArray);
+                    GenericmethodAndclass<double>.ToPrint(doubleArray);
+                    GenericmethodAndclass<char>.ToPrint(charArray);
                     break;
                 default:
                     Console.WriteLine("Please select the program given below");
